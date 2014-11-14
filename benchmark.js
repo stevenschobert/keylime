bad7eb599ae130a7ef1b785de6617942ec426cf6
Steven Schobert <spschobert@gmail.com>
2014-11-14 11:17:05 -0600

    update benchmark file

--- benchmark.js
+++ benchmark.js
@@ -2,25 +2,66 @@ var keylime = require('./');
 var _ = require('lodash');
 
 var secureAttrs = function(model) {
+  model._secureAttrs = null;
+
   model.attrHelper('hidden', function(attr) {
+    model._secureAttrs = true;
     attr.secureAttrs = attr.secureAttrs || {};
     attr.secureAttrs.hidden = true;
   });
 
   model.attrHelper('locked', function(attr) {
+    model._secureAttrs = true;
     attr.secureAttrs = attr.secureAttrs || {};
     attr.secureAttrs.locked = true;
   });
 
   model.attrInit(function(name, value, attr) {
-    if (attr.secureAttrs) {
-      delete this[name];
-      Object.defineProperty(this, name, {
-        value: value,
-        enumerable: !attr.secureAttrs.hidden,
-        configurable: !attr.secureAttrs.locked,
-        writable: !attr.secureAttrs.locked
-      });
+    if (model._secureAttrs) {
+      if (attr.secureAttrs) {
+        delete this[name];
+        Object.defineProperty(this, name, {
+          value: value,
+          enumerable: !attr.secureAttrs.hidden,
+          configurable: !attr.secureAttrs.locked,
+          writable: !attr.secureAttrs.locked
+        });
+      }
+    }
+  });
+};
+
+var secureAttrs2 = function(model) {
+  model._secureAttrs = null;
+
+  model.attrHelper('hidden', function(attr) {
+    model._secureAttrs = true;
+    attr.secureAttrs = attr.secureAttrs || {};
+    attr.secureAttrs.hidden = true;
+  });
+
+  model.attrHelper('locked', function(attr) {
+    model._secureAttrs = true;
+    attr.secureAttrs = attr.secureAttrs || {};
+    attr.secureAttrs.locked = true;
+  });
+
+  model.init(function() {
+    var attr, value;
+    if (model._secureAttrs) {
+      for (var name in model.attrs()) {
+        attr = model.attrs(name);
+        if (attr.secureAttrs) {
+          value = this[name];
+          delete this[name];
+          Object.defineProperty(this, name, {
+            value: value,
+            enumerable: !attr.secureAttrs.hidden,
+            configurable: !attr.secureAttrs.locked,
+            writable: !attr.secureAttrs.locked
+          });
+        }
+      }
     }
   });
 };
@@ -67,10 +108,28 @@ suite('.attrInit', function() {
   });
 });
 
-suite('plugin', function() {
+suite('attrInit plugin', function() {
   var Post = keylime('Post').attr('name', '');
   var Post2 = keylime('Post2').use(secureAttrs).attr('name', '');
   var Post3 = keylime('Post3').use(secureAttrs).attr('name', '').hidden();
+
+  bench('with no plugin', function() {
+    new Post({name: 'test'});
+  });
+
+  bench('with unused plugin', function() {
+    new Post2({name: 'test'});
+  });
+
+  bench('with used plugin', function() {
+    new Post3({name: 'test'});
+  });
+});
+
+suite('init plugin', function() {
+  var Post = keylime('Post').attr('name', '');
+  var Post2 = keylime('Post2').use(secureAttrs2).attr('name', '');
+  var Post3 = keylime('Post3').use(secureAttrs2).attr('name', '').hidden();
 
   bench('with no plugin', function() {
     new Post({name: 'test'});
