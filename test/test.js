c0d287a0f8b1a33c79dbd4a3c20c59d5bbcc26b5
Steven Schobert <spschobert@gmail.com>
2014-12-27 09:10:48 -0600

    use prototype chain for APIs

--- test/test.js
+++ test/test.js
@@ -5,13 +5,29 @@
   var assert = require('assert');
   var _ = require('lodash');
 
-
   describe('utilities', function() {
+
     describe('#createNamedConstructor', function() {
       it('should set the function name using the first parameter', function() {
         var func = keylime.util.createNamedConstructor('Stormtrooper');
         assert.equal(func.name, 'Stormtrooper');
       });
     });
+
+    describe('#convertConstructorToKeylime', function() {
+      it('should change the prototype of the function to KeylimeConstructor', function() {
+        function Jedi() {}
+        keylime.util.convertConstructorToKeylime(Jedi);
+        assert.equal(Jedi.__proto__, keylime.core.KeylimeConstructor.prototype);
+      });
+    });
+
   });
+
+  describe('KeylimeConstructor prototype', function() {
+    it('should have it\'s prototype set to Function.prototype', function() {
+      assert.equal(keylime.core.KeylimeConstructor.prototype.__proto__, Function.prototype);
+    });
+  });
+
 }());
