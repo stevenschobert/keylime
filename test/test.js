5355dd98c0c69b699d17a6c1bdd93e9775727783
Steven Schobert <spschobert@gmail.com>
2014-12-30 08:37:24 -0600

    add clone utility function

--- test/test.js
+++ test/test.js
@@ -83,6 +83,94 @@
         }});
         assert.equal(target.timesCreated, 1);
       });
+
+    describe('#clone', function() {
+      it('should return values for booleans', function() {
+        assert.equal(keylime.util.clone(true), true);
+        assert.equal(typeof keylime.util.clone(true), 'boolean');
+      });
+
+      it('should return values for numbers', function() {
+        assert.equal(keylime.util.clone(2), 2);
+        assert.equal(typeof keylime.util.clone(2), 'number');
+      });
+
+      it('should return values for strings', function() {
+        assert.equal(keylime.util.clone('test'), 'test');
+        assert.equal(typeof keylime.util.clone('test'), 'string');
+      });
+
+      it('should copy boolean objects', function() {
+        /* jshint -W053 */
+        var bool = new Boolean(true);
+        assert.notEqual(bool, keylime.util.clone(bool));
+        assert.equal(bool.valueOf(), keylime.util.clone(bool).valueOf());
+      });
+
+      it('should copy string objects', function() {
+        /* jshint -W053 */
+        var testStr = new String('test');
+        assert.notEqual(testStr, keylime.util.clone(testStr));
+        assert.equal(testStr.valueOf(), keylime.util.clone(testStr).valueOf());
+      });
+
+      it('should copy number objects', function() {
+        /* jshint -W053 */
+        var testNum = new Number(2);
+        assert.notEqual(testNum, keylime.util.clone(testNum));
+        assert.equal(testNum.valueOf(), keylime.util.clone(testNum).valueOf());
+      });
+
+      it('should copy regexp objects', function() {
+        var testRegex = new RegExp('hello.*', 'i');
+        var cloned = keylime.util.clone(testRegex);
+        assert.notEqual(testRegex, cloned);
+        assert.equal(testRegex.toString(), cloned.toString());
+      });
+
+      it('should copy date objects', function() {
+        var testDate = new Date();
+        var cloned = keylime.util.clone(testDate);
+        assert.notEqual(testDate, cloned);
+        assert.equal(testDate.toString(), cloned.toString());
+      });
+
+      it('should copy custom objects', function() {
+        function Droid(model) { this.model = model; }
+        var c3po = new Droid('protocol');
+        var cloned = keylime.util.clone(c3po);
+        assert.notEqual(cloned, c3po);
+        assert.deepEqual(cloned, c3po);
+        assert(cloned instanceof Droid);
+      });
+
+      it('should copy object keys', function() {
+        var testObj = { one: 1, two: 'two' };
+        assert.notEqual(keylime.util.clone(testObj), testObj);
+        assert.deepEqual(keylime.util.clone(testObj), testObj);
+      });
+
+      it('should copy objects with deep copying', function() {
+        var testObj1 = { test: 'test' };
+        var testObj2 = { one: testObj1 };
+        var cloned = keylime.util.clone(testObj2, true);
+        assert.notEqual(cloned.one, testObj1);
+        assert.deepEqual(cloned.one, testObj1);
+      });
+
+      it('should copy array values', function() {
+        var testArr = [ 'one', 2 ];
+        assert.notEqual(keylime.util.clone(testArr), testArr);
+        assert.deepEqual(keylime.util.clone(testArr), testArr);
+      });
+
+      it('should copy array values with deep copying', function() {
+        var testObj1 = { test: 'test' };
+        var testArr = [ testObj1, 2 ];
+        var cloned = keylime.util.clone(testArr, true);
+        assert.notEqual(cloned[0], testObj1);
+        assert.deepEqual(cloned[0], testObj1);
+      });
     });
 
   });
