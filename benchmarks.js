38e015abd2580fe2c8b3deb36fd08ab92a0b232a
Steven Schobert <spschobert@gmail.com>
2014-12-31 10:47:33 -0600

    update benchmarks

--- benchmarks.js
+++ benchmarks.js
@@ -1,51 +1,51 @@
 var keylime = require('./');
 var _ = require('lodash');
 
-var KeylimePost = keylime('KeylimePost');
-  //.attr('title', '')
-  //.attr('data', function createDate() {
-  //  return Date.now();
-  //});
+var KeylimePost = keylime('KeylimePost')
+  .attr('title', '')
+  .attr('data', function createDate() {
+    return Date.now();
+  });
 
 function PlainPost(params) {
-  //var attrs = {
-  //  title: '',
-  //  date: function createDate() {
-  //    return Date.now();
-  //  }
-  //};
-  //params = params || {};
+  var attrs = {
+    title: '',
+    date: function createDate() {
+      return Date.now();
+    }
+  };
+  params = params || {};
 
-  //var sanitizedParams = {};
+  var sanitizedParams = {};
 
-  //for (var attrName in attrs) {
-  //  if (attrs.hasOwnProperty(attrName)) {
-  //    if (params[attrName] !== undefined) {
-  //      sanitizedParams[attrName] = params[attrName];
-  //    } else {
-  //      sanitizedParams[attrName] = attrs[attrName];
-  //    }
-  //  }
-  //}
+  for (var attrName in attrs) {
+    if (attrs.hasOwnProperty(attrName)) {
+      if (params[attrName] !== undefined) {
+        sanitizedParams[attrName] = params[attrName];
+      } else {
+        sanitizedParams[attrName] = attrs[attrName];
+      }
+    }
+  }
 
-  //for (var sanParamName in sanitizedParams) {
-  //  if (sanitizedParams.hasOwnProperty(sanParamName)) {
-  //    if (typeof sanitizedParams[sanParamName] === 'function') {
-  //      this[sanParamName] = sanitizedParams[sanParamName]();
-  //    } else {
-  //      this[sanParamName] = sanitizedParams[sanParamName];
-  //    }
-  //  }
-  //}
+  for (var sanParamName in sanitizedParams) {
+    if (sanitizedParams.hasOwnProperty(sanParamName)) {
+      if (typeof sanitizedParams[sanParamName] === 'function') {
+        this[sanParamName] = sanitizedParams[sanParamName]();
+      } else {
+        this[sanParamName] = sanitizedParams[sanParamName];
+      }
+    }
+  }
 }
 
 suite('creating instances with \'new\'', function() {
   bench('keylime', function() {
-    new KeylimePost();
+    new KeylimePost({title: 'New Post'});
   });
 
   bench('plain constructor', function() {
-    new PlainPost();
+    new PlainPost({title: 'New Post'});
   });
 });
 
